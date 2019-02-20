de601ae6709120784d9d7fea8f1310207347e478
wangpeng <2337865364@qq.com>
2019-02-20 15:04:16 +0800

    2.20d

--- routes/web.php
+++ routes/web.php
@@ -102,3 +102,4 @@ Route::get('/weixin/valid1','Weixin\WeixinController@validToken1');
 Route::post('/weixin/valid1','Weixin\WeixinController@wxEvent');        //接收微信服务器事件推送
 Route::post('/weixin/valid','Weixin\WeixinController@validToken');
 Route::get('/weixin/add','Weixin\WeixinController@createMenu');
+Route::get('/weixin/fsend','Weixin\WeixinController@getMass');
