efb39e9ee3c908f085b5addf3a2d43e12419433a
nihaoa134 <2337865364@qq.com>
2019-03-12 01:36:04 +0000

    3.12

--- app/Http/Controllers/Weixin/WxController.php
+++ app/Http/Controllers/Weixin/WxController.php
@@ -4,75 +4,72 @@ namespace App\Http\Controllers\Weixin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Model\GoodsModel;
 
 class WxController extends Controller
 {
-    public function check(Request $request){
-        echo  $request->input('echostr');
+    public function check(Request $request)
+    {
+        /*        echo  $request->input('echostr');
 
-//        file_put_contents("/tmp/aasd.log",$arr,FILE_APPEND);
-//      $str=$arr['echostr'];
-//        echo $str;
-         $request->input('echostr');
+        //        file_put_contents("/tmp/aasd.log",$arr,FILE_APPEND);
+        //      $str=$arr['echostr'];
+        //        echo $str;
+                 $request->input('echostr');
+                $str = file_get_contents("php://input");
+                //echo $str;
+                file_put_contents('/tmp/weixin.log', $str, FILE_APPEND);
+               $objxml = simplexml_load_string($str);
+                $arr['ToUserName'] = $objxml->ToUserName;
+                $arr['FromUserName'] = $objxml->FromUserName;
+                $arr['CreateTime'] = $objxml->CreateTime;
+                $arr['MsgType'] = $objxml->MsgType;
+                $arr['Event'] = $objxml->Event;
+                if($objxml->MsgType == 'event'){
+                    if($objxml->Event == 'subscribe'){
+                        Xml::insert($arr);
+                    }else{
+                        Xml::where(['FromUserName'=>$objxml->FromUserName,'Event'=>'subscribe'])->delete();
+                    }
+                }*/
+
+
+//图文推送
         $str = file_get_contents("php://input");
-        //echo $str;
-        file_put_contents('/tmp/weixin.log', $str, FILE_APPEND);
-       $objxml = simplexml_load_string($str);
-        $arr = [];
-//        $arr['ToUserName'] = $objxml->ToUserName;
-        $arr['FromUserName'] = $objxml->FromUserName;
-        $arr['CreateTime'] = $objxml->CreateTime;
-        $arr['MsgType'] = $objxml->MsgType;
-        $arr['Event'] = $objxml->Event;
-        if($objxml->MsgType == 'event'){
-            if($objxml->Event == 'subscribe'){
-                Xml::insert($arr);
-            }else{
-                Xml::where(['FromUserName'=>$objxml->FromUserName,'Event'=>'subscribe'])->delete();
-            }
-        }
 
-        if ($objxml->MsgType == 'text') {
-            $goods_name = $objxml->Content;
-            $goods_info = Goods::where('goods_name','like',"%$goods_name%")->get();
+        $objxml = simplexml_load_string($str);
+        $ToUserName = $objxml->ToUserName;
+        $CreateTime = $objxml->CreateTime;
+        $FromUserName = $objxml->FromUserName;
+        $MsgType = $objxml->MsgType;
+        $Event = $objxml->Event;
+        $Content = $objxml->Content;
+        $data = DB::table('shop_goods')->where('goods_name', 'like', "%$Content%")->first();
+        $goods_name = $data->goods_name;
+        $goods_selfmon = $data->goods_selfmon;
+        if ($data) {
+            $goods_img = "http://www.lixiaonitongxue.top/wx/images/$data->goods_img";
+            $title = "$goods_name";
+            $descriptionl = "$goods_selfmon";
             $time = time();
-            $count = count($goods_info);
-            if ($count>0) {
-                $url = "https://www.baidu.com/";
-                $str = "<xml>
-			  <ToUserName><![CDATA[$objxml->FromUserName]]></ToUserName>
-			    <FromUserName><![CDATA[$objxml->ToUserName]]></FromUserName>
-				  <CreateTime>$time</CreateTime>
-				    <MsgType><![CDATA[news]]></MsgType>
-					  <ArticleCount>$count</ArticleCount>
-					    <Articles>";
-
-                foreach ($goods_info as $k => $v) {
-                    $picurl = "http://39.96.199.148/uploads/".$v->goods_img;
-                    $str .= "<item>
-							      <Title><![CDATA[$v->goods_name]]></Title>
-								        <Description><![CDATA[$v->goods_desc]]></Description>
-										      <PicUrl><![CDATA[$picurl]]></PicUrl>
-											        <Url><![CDATA[$url]]></Url>
-													    </item>";
-                }
-                $str .= "</Articles></xml>";
-            } else {
-                $str_info = "您查询的内容不存在";
-                $str = "<xml>
-			  <ToUserName><![CDATA[$objxml->FromUserName]]></ToUserName>
-			    <FromUserName><![CDATA[$objxml->ToUserName]]></FromUserName>
-				  <CreateTime>$time</CreateTime>
-				    <MsgType><![CDATA[text]]></MsgType>
-					  <Content><![CDATA[$str_info]]></Content>
-					  </xml>";
-
-           }
+            $str = "<xml>
+                        <ToUserName><![CDATA[$FromUserName]]></ToUserName>
+                       <FromUserName><![CDATA[$ToUserName]]></FromUserName>
+                        <CreateTime>$time</CreateTime>
+                        <MsgType><![CDATA[news]]></MsgType>
+                        <ArticleCount>1</ArticleCount>
+                        <Articles>
+                    <item>
+                    <Title><![CDATA[$title]]></Title>
+                    <Description><![CDATA[$descriptionl]]></Description>
+                    <PicUrl><![CDATA[$goods_img]]></PicUrl>
+                    <Url><![CDATA[$goods_img]]></Url>
+                    </item>
+                </Articles>
+                </xml>";
             echo $str;
+
+
         }
-
-
-
     }
-
 }
